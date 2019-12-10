d4b8cc0541a4657b0e8d52e72637017f574ecdcd
Anthony <skou3917@gmail.com>
2019-12-10 09:14:02 +0800

    default value of constant columns is done

--- AutoWePo.py
+++ AutoWePo.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[11]:
 
 
 import pandas as pd
 import numpy as np
 import os
+import re
+import datetime
 from tabulate import tabulate
 
 
-# In[2]:
+# In[18]:
 
 
 def _readReport():
     global GlobalVar
-    GlobalVar.reportDf = pd.read_excel(GlobalVar.fileName, header = 0)
-    #replace datetime to only date 
-    for dateColumn in GlobalVar.dateMetadata:
-        GlobalVar.reportDf[dateColumn] = GlobalVar.reportDf[dateColumn].dt.date
-    #set all cell dataType of dataframe to str
-    GlobalVar.reportDf = GlobalVar.reportDf.astype(str)
+    GlobalVar.reportDf = pd.read_excel(GlobalVar.fileName, header = 0, dtype = str)
+#   mark to be deleted
+#     #replace datetime to only date 
+#     for dateColumn in GlobalVar.dateMetadata:
+#         GlobalVar.reportDf[dateColumn] = GlobalVar.reportDf[dateColumn].dt.date
+#     #set all cell dataType of dataframe to str
+#     GlobalVar.reportDf = GlobalVar.reportDf.astype(str)
     #replace nan to empty string
     GlobalVar.reportDf = GlobalVar.reportDf.replace(np.nan, "")
     _reorder()
@@ -30,9 +33,9 @@ def _reorder():
     checkPart = GlobalVar.reportDf.loc[GlobalVar.reportDf['SKILL'] == "Check"].sort_values(by=["AP"])
     complementaryPart = GlobalVar.reportDf.loc[GlobalVar.reportDf['SKILL'] != "Check"].sort_values(by=["OA_NO", "AP", "OA_DESC", "SKILL"])
     if checkPart.size > 0:
-        GlobalVar.reportDf = pd.concat([checkPart, complementaryPart]).reset_index()
+        GlobalVar.reportDf = pd.concat([checkPart, complementaryPart]).reset_index(drop=True)
     else:
-        GlobalVar.reportDf = complementaryPart.reset_index()
+        GlobalVar.reportDf = complementaryPart.reset_index(drop=True)
 #         complementaryPart = GlobalVar.reportDf.loc[GlobalVar.reportDf['SKILL'] != "Check"]
 #         complementaryPart = complementaryPart.sort_values(by=["OA_NO", "AP", "SKILL"])
     
@@ -46,6 +49,9 @@ def _showBrief():
 
 def _saveXlsx():
     global GlobalVar
+    GlobalVar.reportDf["A_DATE"] = _getFirstDayOfWeek()
+    GlobalVar.reportDf["ITEM"] = np.arange(len(GlobalVar.reportDf)) + 1
+    GlobalVar.reportDf["OWNER"] = GlobalVar.owner
     GlobalVar.reportDf.to_excel(GlobalVar.fileName, index = False)
     
 def _controller():
@@ -56,10 +62,10 @@ def _controller():
     while(True):
         action = input("To do? ").lower().strip()
         if action in GlobalVar.functionDict:
-#             try:
+            try:
                 GlobalVar.functionDict[action]()
-#             except:
-#                 print("Unexpected error:", sys.exc_info()[0])
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
         elif action == "?":
             for key in GlobalVar.functionDict:
                 print(key)
@@ -68,7 +74,11 @@ def _controller():
         else:
             print("Unknow function")
             
-
+def _getFirstDayOfWeek(gap = "/"):
+    date = datetime.datetime.today()
+    start = date - datetime.timedelta(days=date.weekday())
+    return start.strftime(f"%Y{gap}%m{gap}%d")
+            
 def displayAll():
     global GlobalVar
     print(tabulate(GlobalVar.reportDf, headers='keys', tablefmt='psql'))
@@ -125,22 +135,89 @@ def removeRow():
     else:
         print("canceled")
 
+def _selfCheck():
+    global GlobalVar
+    if not os.path.isfile("person.config"):
+        firstExecute()
+    _readConfig()
+    
+    if GlobalVar.owner == None:
+        print("ERROR: do not get owner name")
+        return False
+    
+    if GlobalVar.fileName != None:
+        _readReport()
+    else:
+        GlobalVar.fileName = f"WeeklyReport-V1.0-{_getFirstDayOfWeek('.')}-{GlobalVar.owner}.xlsx"
+        pattern = re.compile(r"(?P<key>[a-zA-Z1-9]*)=(?P<value>.*)")
+        with open("person.config", "r+") as config:
+            pattern = re.compile(r"(?P<key>[a-zA-Z1-9]*)=(?P<value>.*)")
+            with open("person.config", "r+") as config:
+                lines = config.readlines()
+                for index, line in enumerate(lines):
+                    matcher = pattern.match(line.strip())
+                    if matcher:
+                        key = matcher.group("key")
+                        value = matcher.group("value")
+                        if key == "fileName":
+                            lines[index] = f"fileName={GlobalVar.fileName}\n"
+                config.seek(0)
+                config.writelines(lines)
+        _saveXlsx()
+    return True
+
+def firstExecute():
+    global GlobalVar
+    print("First time execute, setting...")
+    with open("person.config", "w") as newConfig:
+        newConfig.write("owner=" + input("Owner Name? ").strip() + "\n")
+
+        newConfig.write("fileName=" + input("import weekly report? (keep empty if no) file name: ") + "\n")
+
+        newConfig.write("simpleDisplayColumn="  + ", ".join(list(map(str,GlobalVar.displayColumns))) + "\n")
+    print("Setting Ok")
+
+def _readConfig():
+    global GlobalVar
+    pattern = re.compile(r"(?P<key>[a-zA-Z1-9]*)=(?P<value>.*)")
+    with open("person.config", "r") as config:
+        try:
+            while True:
+                content = next(config)
+                matcher = pattern.match(content.strip())
+                if matcher:
+                    key = matcher.group("key")
+                    value = matcher.group("value")
+                    if key == "owner":
+                        GlobalVar.owner = value
+                    if key == "fileName":
+                        if value is "":
+                            GlobalVar.reportDf = pd.DataFrame(columns = GlobalVar.metadata)
+                        else:
+                            GlobalVar.fileName = value
+                    if key == "simpleDisplayColumn":
+                        GlobalVar.displayColumns = list(map(int, list(map(str.strip, value.split(",")))))
+        except StopIteration: # EOF
+            pass
+        
+    
+
 class GlobalVar():
     reportDf = None
     metadata =  ['A_DATE', 'ITEM', 'OA_DESC', 'AP', 'SKILL', 'SITE', 'DUE_DATE', 'COMPLET_D', 'OWNER', 'IT_STATUS', 'OA_NO', 'PROGRAM', 'W_HOUR', 'REMARK', 'PROG_CNT', 'OA_STATUS']
     constMetadata = ['A_DATE', 'ITEM', 'OWNER']
     dateMetadata = ['DUE_DATE', 'COMPLET_D']
-    displayColumns = [2, 12, 13]
+    displayColumns = [2, 12, 13, 15]
     functionDict = {"new": addNewRow, "all": displayAll, "save": saveExcel, "edit": editRow, "remove": removeRow}
-#     fileName = "WeeklyReport-V1.0-2019.09.30-TonyOu.xlsx"
-    fileName = "new.xlsx"
+    owner = None
+    fileName = None
     
 def initializeApp():
-    _readReport()
-    _controller()
+    if(_selfCheck()):
+        _controller()
 
 
-# In[3]:
+# In[21]:
 
 
 initializeApp()
