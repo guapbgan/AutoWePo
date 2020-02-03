c36723ca4e962bb33b9c8d771ae15c1f7ec2047f
Anthony <skou3917@gmail.com>
2020-02-03 11:39:54 +0800

    update bin

--- RUN_ME.bat
+++ RUN_ME.bat
@@ -1,2 +1,3 @@
-rem cmd /c "bin\AutoWePo.exe" 
-C:\Users\Admin\AppData\Local\Programs\Python\Python37\python.exe build\AutoWePo.py
+cmd /c "bin\AutoWePo.exe" 
+rem C:\Users\Admin\AppData\Local\Programs\Python\Python37\python.exe build\AutoWePo.py
+pause
